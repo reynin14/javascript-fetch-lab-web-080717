e3960e053b799ad6c86f6c7691649ec7fe4b6e91
github username <github email address>
2017-09-25 16:45:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 function getIssues() {
+  let token = getToken();
+  const repo = 'reynin14/javascript-fetch-lab'
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    headers: {
+      Authorization: `token ${token}`
+    },
+  }).then(res => res.json())
+  .then(json => console.log(json))
 }
 
 function showIssues(json) {
 }
 
 function createIssue() {
+  let token = getToken()
+  const repo = 'reynin14/javascript-fetch-lab'
+  let body = document.getElementById('body').value
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    headers: {
+      Authorization: `token ${token}`
+    },
+    method: 'post',
+    body: JSON.stringify(body)
+  }).then(res => res.json())
+  .then(json => console.log(json))
+  debugger
 }
 
 function showResults(json) {
+  const issueDiv = document.querySelector()
 }
 
 function forkRepo() {
+  let token = getToken()
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/fork`, {
+    headers: {
+      Authorization: `token ${token}`
+    },
+    method: 'post'
+  }).then(res => res.json())
+  .then(json => console.log(json));
+}
+
+function showForkedRepo(resp){
+
 }
 
 function getToken() {
